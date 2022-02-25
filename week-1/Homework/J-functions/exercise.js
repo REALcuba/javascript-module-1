44a55943426d260243f1bd35f5b01c6305ccd77c
REALcuba <angelskcuba@gmail.com>
2022-02-26 00:48:14 +0100

    homework j

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,14 @@
 function halve(number) {
   // complete the function here
+  return number/2;
 }
 
 var result = halve(12);
-
+const result1 = halve(26);
+const result2 = halve(89)
 console.log(result);
+console.log(result1);
+console.log(result2);
+
+
+
