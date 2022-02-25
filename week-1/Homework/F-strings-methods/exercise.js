ec63fabda1929733a2e95381036d536ca5641e20
REALcuba <angelskcuba@gmail.com>
2022-02-26 00:42:14 +0100

    homework F

--- week-1/Homework/F-strings-methods/exercise.js
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
 
+const myName = "Angel";
+const nameLength = myName.length
+    
+
+const message = `my name is ${myName}, it has ${nameLength} letters`;
+
 console.log(message);
--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,11 @@
-const name = " Daniel  ";
+const myName = " Daniel  ";
+const nameLength = myName.length;
+const message = `my name is ${myName}, it has ${nameLength} letters`;
 
 console.log(message);
+const trimedName = myName.trim(myName);
+
+const trimMessage = `my name is ${trimedName}, it has ${nameLength} letters`;
+
+
+console.log(trimMessage);
