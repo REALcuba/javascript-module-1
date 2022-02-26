61b88e213569a7ae227be81cac1c33f62a2a0c5d
REALcuba <angelskcuba@gmail.com>
2022-02-26 11:25:50 +0100

    homework L

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,15 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function getShoutyGreeting(name) {
+
+     ShoutyName = name.toUpperCase();
+    
+     return `HELLO ${ShoutyName}`;
+}
+console.log(getShoutyGreeting(mentor1));
+console.log(getShoutyGreeting(mentor2));
+console.log(getShoutyGreeting(mentor3));
+console.log(getShoutyGreeting(mentor4));
+console.log(getShoutyGreeting(mentor5));
