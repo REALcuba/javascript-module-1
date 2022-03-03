ce11a248fb12962c3a6dfeb18f28f019c8651ebb
REALcuba <angelskcuba@gmail.com>
2022-03-03 19:24:07 +0100

    exercise B and C done

--- week-2/InClass/exercise-C.js
+++ week-2/InClass/exercise-C.js
@@ -1,11 +1,17 @@
+// declare numberChecker function
 function numberChecker(num) {
+  //  if num is bigger than 20 , it return ${num} is greater than 20
   if (num > 20) {
     return `${num} is greater than 20`;
+    //  if is strict equal 20, it return ${num} is equal to 20
   } else if (num === 20) {
     return `${num} is equal to 20`;
+    //  if num is smaller than 20, it return `${num} is less than 20`;
   } else if (num < 20) {
     return `${num} is less than 20`;
+    //  if num is a string, it  return `${num} isn't even a number :(`
   } else {
     return `${num} isn't even a number :(`;
   }
 }
+console.log(numberChecker("20"));
