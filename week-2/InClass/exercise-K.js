93945683f8028149a07a66e8e418a9537823f89c
REALcuba <angelskcuba@gmail.com>
2022-03-05 16:37:56 +0100

    inClass K done

---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,10 @@
+let studentArray = ["pepe", "juan", "carlos"]
+
+function printStudentsName(arr) {
+ for (let i = 0; i < arr.length; i++){
+ const student= arr[i]
+console.log(student);
+} 
+}
+
+printStudentsName(studentArray)
