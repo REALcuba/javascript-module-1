ee545d0cfaf0f167c419b5641e5495c52096954e
REALcuba <angelskcuba@gmail.com>
2022-03-09 19:56:46 +0100

    week 3 inClass A

---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,14 @@
+let papas =["pepe", "carlos", "juan", "jorge", "angel"];
+ function papasName(arr) {
+     
+     let papitos = arr.length;
+     console.log(papas[0]);
+ console.log(`We have ${papitos} papitos`);
+ }
+
+ papasName(papas);
+ console.log(papas);
+ console.log(papas.unshift("coco")) ;
+ console.log(papas);
+ console.log(papas.push("ozzy"));
+ console.log(papas);
