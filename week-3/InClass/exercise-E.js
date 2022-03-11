85f1dd882949953be9959df76880cc5dc5fd4c3c
REALcuba <angelskcuba@gmail.com>
2022-03-11 20:20:42 +0100

    week 3 inClass E done

---
+++ week-3/InClass/exercise-E.js
@@ -0,0 +1,13 @@
+const yearArray =[1964, 2008, 1999, 2005, 1978, 1985, 1919];
+
+function ageFinder(birthYear) {
+const age = 2022 - birthYear 
+console.log(age) 
+}
+
+// doubt abot this
+ const ageArr =  yearArray.forEach(ageFinder)
+ 
+
+   
+
