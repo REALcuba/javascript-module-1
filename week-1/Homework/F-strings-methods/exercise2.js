27037ef784ebbf2a5c09d294cd32151344c8e725
REALcuba <angelskcuba@gmail.com>
2022-04-12 22:59:13 +0200

    js m1 w1 hf done

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -5,7 +5,6 @@ const message = `my name is ${myName}, it has ${nameLength} letters`;
 console.log(message);
 const trimedName = myName.trim();
 
-const trimMessage = `my name is ${trimedName}, it has ${trimedName.length} letters`;
-
+const trimMessage = `my name is ${trimedName}, and my name is ${trimedName.length} characters long`;
 
 console.log(trimMessage);
